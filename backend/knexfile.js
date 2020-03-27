e1ea910b3814d978fe982393e0f5ff60002577e5
MuriloCicolin <mu_cicolin@hotmail.com>
2020-03-27 15:22:33 -0300

    testes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,6 +5,17 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
       filename: './src/database/db.sqlite'
     },
     migrations: {
